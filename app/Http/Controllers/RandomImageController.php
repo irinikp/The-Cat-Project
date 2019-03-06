f7612f0cbca9a52e04cd3325d37fda00b8f051e6
Irini Koutaki <irinikp@gmail.com>
2019-03-06 15:59:01 +0200

    temporarily stop calling API

--- app/Http/Controllers/RandomImageController.php
+++ app/Http/Controllers/RandomImageController.php
@@ -13,8 +13,9 @@ class RandomImageController extends Controller
      */
     public function index()
     {
-        $image_searcher = new CatImages();
-        $response = $image_searcher->search(400);
+//        $image_searcher = new CatImages();
+//        $response = $image_searcher->search(400);
+        $response = "http://www.texasforthem.org/wp-content/uploads/2016/03/Feral-Cat-1.jpg";
         return response()
             ->view('random-image', ['response' => $response], 200);
     }
