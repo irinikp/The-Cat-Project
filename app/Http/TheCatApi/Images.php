f616bbc4ea49efe7d3dcd2f6ca615e30c4acbe39
Irini Koutaki <irinikp@gmail.com>
2019-03-06 11:47:19 +0200

    Create TheCatApi Class

---
+++ app/Http/TheCatApi/Images.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: irinikoutaki
+ * Date: 2019-03-06
+ * Time: 11:28
+ */
+
+namespace App\Http\TheCatApi;
+
+
+class Images extends Integrator
+{
+    const CLASS_URL = "images/";
+
+    public function search($size = 'full', $mime_types = 'jpg,png,gif', $format = 'json', $order = 'RANDOM', $page = 0,
+                           $limit = 1, $category_ids = '', $breed_ids = '')
+    {
+        $url = self::BASE_URL . self::CLASS_URL . "search?size=$size&mime_types=$mime_types&format=$format&order=$order&page=$page"
+            . "&limit=$limit&category_ids=$category_ids&breed_ids=$breed_ids";
+        $response = $this->http_response($url);
+        $url = $response[0]->url;
+        return $url;
+    }
+
+}
---
+++ app/Http/TheCatApi/Integrator.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: irinikoutaki
+ * Date: 2019-03-06
+ * Time: 11:33
+ */
+
+namespace App\Http\TheCatApi;
+
+
+class Integrator
+{
+    const BASE_URL = "https://api.thecatapi.com/v1/";
+
+    protected function http_response($url, $status = null, $wait = 3)
+    {
+        $failed_response = '';
+        $time = microtime(true);
+        $expire = $time + $wait;
+
+        // we fork the process so we don't have to wait for a timeout
+        $pid = pcntl_fork();
+        if ($pid == -1) {
+            die('could not fork');
+        } else if ($pid) {
+            // we are the parent
+            $ch = curl_init();
+            $request_headers = array('x-api-key:63d6ed7a-1f53-4467-82d7-28795354849c');
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $request_headers);
+
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+            $head = curl_exec($ch);
+            $response = json_decode($head);
+//            $url = $response[0]->url;
+            $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+
+            if(!$head)
+            {
+                return $failed_response;
+            }
+
+            if($status === null)
+            {
+                if($httpCode < 400)
+                {
+                    return $response;
+                }
+                else
+                {
+                    return $failed_response;
+                }
+            }
+            elseif($status == $httpCode)
+            {
+                return $response;
+            }
+
+            return $failed_response;
+            pcntl_wait($status); //Protect against Zombie children
+        } else {
+            // we are the child
+            while(microtime(true) < $expire)
+            {
+                sleep(0.5);
+            }
+            return $failed_response;
+        }
+    }
+
+}
