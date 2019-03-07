c1dd7b85be0818e1eea7a7fe32c0deff2811b1b8
Irini Koutaki <irinikp@gmail.com>
2019-03-07 10:51:06 +0200

    remove default menu items

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,12 +86,6 @@
 
                 <div class="links">
                     <a href="/random-image">Random Image</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
